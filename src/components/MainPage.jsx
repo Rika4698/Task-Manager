e2e79df52bb5ffc7e2c56fb644dec627a350ed81
Rika4698 <rikasharmin181@gmail.com>
2026-01-28 01:21:35 +0600

    create add task with text

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,14 +1,30 @@
+import { useState } from "react";
 import AddTask from "./Section/AddTask";
 import Header from "./Section/Header";
 
 
 const MainPage = () => {
+    const [task, setTask] = useState([]);
+
+
+// Add new task
+    const addTask = (text) => {
+        const newTask ={
+            id:Date.now(),
+            text:text,
+            completed:false,
+         
+        };
+        setTask([newTask, ...task]);
+    };
+
+
     return (
         <div className="max-w-4xl mx-auto ">
             <Header/>
 
             <div>
-           <AddTask />
+           <AddTask addTask={addTask}/>
 
             </div>
             
--- src/components/Section/AddTask.jsx
+++ src/components/Section/AddTask.jsx
@@ -1,12 +1,27 @@
+import { useState } from "react";
 
 
-const AddTask = () => {
+const AddTask = ({addTask}) => {
+    const[text, setText] = useState('');
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        const trimText = text.trim();
+
+        if(trimText === "") return;
+
+       addTask(trimText);
+       setText("");
+
+    };
+
+
     return (
         <div>
 
-            <form className="flex flex-col sm:flex-row gap-2 md:gap-3">
+            <form onSubmit={handleSubmit} className="flex flex-col sm:flex-row gap-2 md:gap-3">
 
-                <input type="text" placeholder="Add New Task" className="input-task py-2.5 px-4 w-full text-sm md:test-base flex-1" autoFocus/>
+                <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Add New Task" className="input-task py-2.5 px-4 w-full text-sm md:test-base flex-1" autoFocus/>
 
                 <button type="submit" className="add-task px-4 whitespace-nowrap w-full sm:w-auto text-sm md:text-base py-2.5 md:py-2 rounded-lg shadow hover:shadow-md transition-all duration-200 active:scale-95">
                Add Task
