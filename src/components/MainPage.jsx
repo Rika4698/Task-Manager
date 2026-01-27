198734a4c8f954a12959f2bb5c734d30b8fbb7bd
Rika4698 <rikasharmin181@gmail.com>
2026-01-28 01:07:17 +0600

    create add task layout

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -4,7 +4,7 @@ import Header from "./Section/Header";
 
 const MainPage = () => {
     return (
-        <div className="max-w-6xl mx-auto ">
+        <div className="max-w-4xl mx-auto ">
             <Header/>
 
             <div>
--- src/components/Section/AddTask.jsx
+++ src/components/Section/AddTask.jsx
@@ -4,7 +4,13 @@ const AddTask = () => {
     return (
         <div>
 
-            <form action="">
+            <form className="flex flex-col sm:flex-row gap-2 md:gap-3">
+
+                <input type="text" placeholder="Add New Task" className="input-task py-2.5 px-4 w-full text-sm md:test-base flex-1" autoFocus/>
+
+                <button type="submit" className="add-task px-4 whitespace-nowrap w-full sm:w-auto text-sm md:text-base py-2.5 md:py-2 rounded-lg shadow hover:shadow-md transition-all duration-200 active:scale-95">
+               Add Task
+                </button>
 
             </form>
             
